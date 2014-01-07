143361f9ccfe24e4d0f6a43ab248aa2ac797d1ef
luo-chengwei <ChengweiLuo@Chengweis-Mac-mini.local>
2014-01-07 10:50:13 -0500

    updated AssemblyStat

--- metagenomics/AssemblyStat.py
+++ metagenomics/AssemblyStat.py
@@ -29,6 +29,20 @@ def assemblyStat(infile):
 		if acc_length >= 0.75 * totalLength and n75 == 0:
 			n75 = x
 	GC = 100 * float(GC_count)/totalLength
-	return (GC, totalLength, len(lengths), lengths[0], max_contig, n25, n50, n75)
+	return (GC, totalLength, len(lengths), max_contig, n25, n50, n75)
 
 
+def main():
+	GC, totalLength, num_contigs, max_contig, n25, n50, n75 = assemblyStat(sys.argv[1])
+	sys.stdout.write('GC%: %.4f\n' % GC)
+	sys.stdout.write('Total length: %i\n' % totalLength)
+	sys.stdout.write('Number of contigs: %i\n' % num_contigs)
+	sys.stdout.write('Max contig length: %i\n' % max_contig)
+	sys.stdout.write('N25: %i\n' % n25)
+	sys.stdout.write('N50: %i\n' % n50)
+	sys.stdout.write('N75: %i\n' % n75)
+	
+if __name__ == '__main__':
+	main()
+	
+	
